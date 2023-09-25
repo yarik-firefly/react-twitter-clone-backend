f52939b50445c7361e45b146f66e733544d66633
yarik-firefly <snowboardvgostahu@gmail.com>
2023-09-25 23:47:55 +0300

    fix

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -6,12 +6,13 @@ export default (req, res, next) => {
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.SECRET_KEY);
+      console.log(req.userId);
 
       req.userId = decoded.data;
       next();
     } catch (error) {
       return res.status(401).json({
-        status: "error",
+        status: error,
         message: "Токен не прочитан!",
       });
     }
