235d9b4e5d92c318e2d4fe7ab4580243e8791245
yarik-firefly <snowboardvgostahu@gmail.com>
2023-09-25 16:27:01 +0300

    пробуй исправить блокировку по корс

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
+
 // const storage = multer.diskStorage({
 //   destination: function (_, __, cb) {
 //     cb(null, __dirname + "/uploads");
