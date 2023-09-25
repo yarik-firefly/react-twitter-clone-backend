e9564a52543790142560e1f48ad9c431bf2583c7
yarik-firefly <snowboardvgostahu@gmail.com>
2023-09-25 16:42:29 +0300

    пробуй исправить блокировку по корс

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import { UploadCtrl } from "./controllers/UploadFilesController.js";
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 
@@ -46,50 +46,50 @@ const upload = multer({ storage: storage });
 app.use(passport.initialize());
 
 app.get(
-  "/users/me",
+  "/users/me/",
   // passport.authenticate("jwt", { session: false }),
   checkAuth,
   UserCtrl.getUserInfo
 );
-app.get("/users", UserCtrl.index);
-app.post("/auth/register", registerValidations, UserCtrl.create);
-app.get("/auth/verify", registerValidations, UserCtrl.verify);
-app.get("/users/:id", UserCtrl.show);
+app.get("/users/", UserCtrl.index);
+app.post("/auth/register/", registerValidations, UserCtrl.create);
+app.get("/auth/verify/", registerValidations, UserCtrl.verify);
+app.get("/users/:id/", UserCtrl.show);
 
 app.post(
-  "/auth/login",
+  "/auth/login/",
   passport.authenticate("local", {
     session: false,
   }),
   UserCtrl.afterLogin
 );
 
-app.get("/tweets", TweetsCtrl.index);
-app.get("/tweet/:id", TweetsCtrl.show);
-app.get("/users/tweets/:id", TweetsCtrl.getUserTweets);
+app.get("/tweets/", TweetsCtrl.index);
+app.get("/tweet/:id/", TweetsCtrl.show);
+app.get("/users/tweets/:id/", TweetsCtrl.getUserTweets);
 app.post(
-  "/tweet",
+  "/tweet/",
   // passport.authenticate("jwt"),
   checkAuth,
   createTweetsValidations,
   TweetsCtrl.create
 );
 app.delete(
-  "/tweet/:id",
+  "/tweet/:id/",
   // passport.authenticate("jwt"),
   checkAuth,
   createTweetsValidations,
   TweetsCtrl.delete
 );
 app.patch(
-  "/tweet/:id",
+  "/tweet/:id/",
   // passport.authenticate("jwt"),
   checkAuth,
   createTweetsValidations,
   TweetsCtrl.update
 );
 
-app.post("/upload", upload.single("image"), UploadCtrl.upload);
+app.post("/upload/", upload.single("image"), UploadCtrl.upload);
 
 app.listen(process.env.PORT || 8888, () => {
   mongoose.connect(process.env.MONGO_DB);
