6b1bb8c2cb605b6c3212484eddaeb2d477820eb4
yarik-firefly <snowboardvgostahu@gmail.com>
2023-09-25 23:19:41 +0300

    fix

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,19 +1,18 @@
 import jwt from "jsonwebtoken";
 
 export default (req, res, next) => {
-  const token = (req.headers.Authorization || "").replace(/Bearer\s?/, "");
+  const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
 
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.SECRET_KEY);
 
       req.userId = decoded.data;
-      // console.log(req.userId)
       next();
     } catch (error) {
       return res.status(401).json({
         status: "error",
-        message: "Нет доступа!",
+        message: "Токен не прочитан!",
       });
     }
   } else {
