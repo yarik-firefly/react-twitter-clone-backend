560b376f8b0747efd5b24eb0f2ec5156af417bb9
yarik-firefly <snowboardvgostahu@gmail.com>
2023-09-25 20:57:23 +0300

    make it better

--- index.js
+++ index.js
@@ -60,27 +60,27 @@ const upload = multer({ storage: storage });
 app.use(passport.initialize());
 
 app.get(
-  "/users/me/",
+  "/users/me",
   // passport.authenticate("jwt", { session: false }),
   checkAuth,
   UserCtrl.getUserInfo
 );
-app.get("/users/", UserCtrl.index);
-app.post("/auth/register/", registerValidations, UserCtrl.create);
-app.get("/auth/verify/", registerValidations, UserCtrl.verify);
-app.get("/users/:id/", UserCtrl.show);
+app.get("/users", UserCtrl.index);
+app.post("/auth/register", registerValidations, UserCtrl.create);
+app.get("/auth/verify", registerValidations, UserCtrl.verify);
+app.get("/users/:id", UserCtrl.show);
 
 app.post(
-  "/auth/login/",
+  "/auth/login",
   passport.authenticate("local", {
     session: false,
   }),
   UserCtrl.afterLogin
 );
 
-app.get("/tweets/", TweetsCtrl.index);
-app.get("/tweet/:id/", TweetsCtrl.show);
-app.get("/users/tweets/:id/", TweetsCtrl.getUserTweets);
+app.get("/tweets", TweetsCtrl.index);
+app.get("/tweet/:id", TweetsCtrl.show);
+app.get("/users/tweets/:id", TweetsCtrl.getUserTweets);
 app.post(
   "/tweet/",
   // passport.authenticate("jwt"),
@@ -89,21 +89,21 @@ app.post(
   TweetsCtrl.create
 );
 app.delete(
-  "/tweet/:id/",
+  "/tweet/:id",
   // passport.authenticate("jwt"),
   checkAuth,
   createTweetsValidations,
   TweetsCtrl.delete
 );
 app.patch(
-  "/tweet/:id/",
+  "/tweet/:id",
   // passport.authenticate("jwt"),
   checkAuth,
   createTweetsValidations,
   TweetsCtrl.update
 );
 
-app.post("/upload/", upload.single("image"), UploadCtrl.upload);
+app.post("/upload", upload.single("image"), UploadCtrl.upload);
 
 app.listen(PORT, () => {
   // mongoose.connect(process.env.MONGO_DB);
